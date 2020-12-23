dbbb146b91b236901bf6e9ea459f9b9c0c0329ea
Shahab Athar <shahabathar103@gmail.com>
2020-12-23 16:38:18 +0500

    Add Advertisement

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Home() {
     return (
         <div>
+            <i class="fab fa-youtube"></i>
             <i class="fab fa-facebook"></i>
-            <i class="fab fa-facebook"></i>
-            <i class="fab fa-facebook"></i>
-            <i class="fab fa-facebook"></i>
-            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram-square"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fas fa-envelope-square"></i>
         </div>
     )
 }
